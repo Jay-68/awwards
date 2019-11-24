429f9d7d81f1e9bddfa68c463826a393ff26c0b4
Jay-68 <kelvinngari1995@gmail.com>
2019-11-24 10:36:43 +0300

    Register application models to the admin

--- awwards/admin.py
+++ awwards/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from awwards.models import Profile, Project, Review
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Review)
