46dba899541f513cfcc13cf4b380c33e7f930ef6
Jay-68 <kelvinngari1995@gmail.com>
2019-11-24 11:11:22 +0300

    Add a forms file for user signin,profile,project and review forms

---
+++ awwards/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from aww.models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+# sign up forms
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        # profile forms
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user', 'timestamp']
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user', 'profile', 'timestamp']
+# review
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = ['user', 'project', 'average']
