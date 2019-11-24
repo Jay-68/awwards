df1d84bf265fd19aa552ee81db9ef23d9877743c
Jay-68 <kelvinngari1995@gmail.com>
2019-11-24 15:37:40 +0300

    Add registration form and routing to log in form

--- awwards/models.py
+++ awwards/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from tinymce.models import HTMLField
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -16,6 +18,15 @@ class Profile(models.Model):
     contact = models.CharField(max_length=50, blank=True)
     timestamp = models.DateTimeField(default=timezone.now())
 
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
     def __str__(self):
         return self.user.username
 
@@ -27,7 +38,8 @@ class Profile(models.Model):
 
     @classmethod
     def filter_by_id(cls, id):
-        profile = Profile.objects.filter_by(user=id).first()
+        profile = Profile.objects.filter(user = id).first()
+        return profile
 
         return profile
 
